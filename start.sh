eb41591afccb34f12b71e60d325c0b57dda10355
Hacesgd <162230713+Hacefr@users.noreply.github.com>
2026-09-11 22:30:26 -0400

    Update start.sh to launch Waterfall directly on port 10000

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "==> [Gateway Door] Starting Node.js HTTP/WebSocket bridge on port 10000..."
-node gateway.js &
-
-echo "==> [Gateway Door] Launching Java Waterfall engine on port 25577..."
-exec java -Xms128M -Xmx384M -Djava.net.preferIPv4Stack=true -Dbungee.native=false -XX:+UseG1GC -jar Waterfall.jar --noconsole
+echo "==> [Gateway Door] Launching Waterfall + Eaglercraft directly on port 10000..."
+exec java -Xms128M -Xmx384M -Djava.net.preferIPv4Stack=true -XX:+UseG1GC -jar Waterfall.jar --noconsole
